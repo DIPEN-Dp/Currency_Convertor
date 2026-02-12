dd96050f9508e40f4b1b0aace87dccc0f10840c2
DIPEN-Dp <yourgithubemail@gmail.com>
2026-02-12 20:27:36 +0530

    Fix bug.

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -31,7 +31,7 @@ const InputBox = ({label,
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
-                    onChange={(e)=> onCurrencyChange && onAmountchannge(e.target.value)} 
+                    onChange={(e)=> onCurrencyChange &&  onCurrencyChange(e.target.value)} 
                     disabled={currencyDisable}
                     
                 >
